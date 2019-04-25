51e6198ea5e21d8503d0c6c32d02c92ae4cf34a6
Griffin <gfluet5@gmail.com>
2019-04-25 08:46:45 -0400

    added test
    test class added

--- src/Testing.java
+++ src/Testing.java
@@ -1,14 +1,30 @@
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Scanner;
-
-public class Main {
+public class Testing {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String input = " ";
-		input = scan.nextLine();	
-		scan.close();
+		
+		// large test 25 columns
+		test("1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 11 11 12 12 13 13 14 14 15 15 16 16 17 17 18 18 19 19 20 20 21 21 22 22 23 23 24 24 25 25",false);
+		// 10 column test
+		test("1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10",false);
+		// 5 column test
+		test("1 1 2 2 3 3 4 4 5 5",false);
+		// packet example
+		test("4 7 1 1 5 9 2 3",true);
+		// impossible test
+		test("1 9 2 1 3 2 4 3",false);
+		// too few columns test 
+		test("1 2 2 1",false);
+		
+	}
+	//pass in boolean to see combos
+	public static void test(String in, boolean results) {
+		
+		long start;
+		long end;
+		String input = in;
 		ArrayList<Column> columns = new ArrayList<Column>();
 		Scanner read = new Scanner(input);
 		while(read.hasNextInt()) {
@@ -25,36 +41,36 @@ public class Main {
 		ArrayList<ArrayList<Column>> lines = new ArrayList<ArrayList<Column>>();
 		
 		Collections.sort(columns, new CompareColumn());
-		
+		start = System.nanoTime();
 		for(Column current : columns) {
 			findLines(lines, new ColumnLine(current), columns);
 		}
 		
 		System.out.println(lines.size());
-		for(ArrayList<Column> alp : lines) {
-			for(Column pil : alp) {
-				System.out.print(pil.getRadius()+" ");
+		if(results) {
+			for(ArrayList<Column> alp : lines) {
+				for(Column pil : alp) {
+					System.out.print(pil.getRadius()+" ");
+				}
+				System.out.println();
 			}
-			System.out.println();
 		}
-		
+		end = System.nanoTime();
+		System.out.println("Running time: " + (end - start));
 	}
-	
 	public static void findLines(ArrayList<ArrayList<Column>> lines, ColumnLine cl, ArrayList<Column> cList) {
 
 		ColumnLine line = cl;
 		ArrayList<Column> current = new ArrayList<Column>();
 		current.addAll(cList);
 		
-
 		if(line.getLine().size() < 4) {
 
 			Column recent = line.getLine().get(line.getLine().size()-1);
 			int index = current.indexOf(recent);
 			
-			for(int i = 0; i <= index; i++) {
+			for(int i = 0; i <= index; i++)
 				current.remove(0);
-			}
 			
 			for(Column cur : current) {
 				ColumnLine temp = new ColumnLine();
@@ -68,5 +84,4 @@ public class Main {
 		else if(line.getLine().size()==4 && line.valid()) lines.add(line.getLine());
 	}
 
-	
 }
